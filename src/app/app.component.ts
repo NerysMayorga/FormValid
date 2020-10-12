7efd757f67377e4ad30227e3e4944d6059e056c8
NerysMayorga <50004026+NerysMayorga@users.noreply.github.com>
2020-10-11 21:03:00 -0600

    Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormValid';
+  angForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    console.log('AppComponent::constructor');
+    this.createForm();
+}
+createForm(): void {
+  console.log('AppComponent::createForm');
+  this.angForm = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(5)]],
+    surname: ['', Validators.required],
+    edad: [0, Validators.required],
+    web: ['', Validators.required],
+  });
+
+  const newLocal = 'name';
+  this.angForm.controls[newLocal].valueChanges.subscribe(data => {
+    console.log(data);
+  });
+}
+onSubmit(): void {
+  if (this.angForm.valid) {
+    console.log(this.angForm.value);
+  } else {
+    alert('ERROR!');
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
